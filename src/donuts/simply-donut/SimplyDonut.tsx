6de1cd4aa4800eae7522a955f5a7f50820f3d13b
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2023-11-03 18:48:19 +0100

    feat(Colors): Palette of 12 default colors if color no specified

--- src/donuts/simply-donut/SimplyDonut.tsx
+++ src/donuts/simply-donut/SimplyDonut.tsx
@@ -5,6 +5,7 @@ import {
   TOTAL_DEGREES,
   TOTAL_PERCENTAGE,
   INITIAL_DEGREE,
+  COLORS,
 } from "./types";
 import { Donut } from "../donut/Donut";
 import { clsx } from "clsx";
@@ -22,21 +23,38 @@ const convertToPercent = (value: number, total: number) =>
 const convertToDegrees = (value: number) =>
   Math.round((value / TOTAL_PERCENTAGE) * TOTAL_DEGREES);
 
+const isHexaFormat = (couleur: string): boolean => {
+  const regex = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/;
+
+  return regex.test(couleur);
+};
+
+const getColor = (index: number, color?: string): string => {
+  return color && isHexaFormat(color) ? color : COLORS[index];
+};
+
 const calculDegreesForDonut = (donutData: TDonutData[]) => {
   const clonedDonutData: TDonutData[] = [...donutData];
   const total = calculateTheTotalValue(clonedDonutData);
+  const sortData = clonedDonutData.sort((a, b) => a.value - b.value);
+  let i = 0;
 
-  return clonedDonutData.reduce(
+  return sortData.reduce(
     (dataForDonut: TDataForDonut[], current: TDonutData, index: number) => {
       const lastEntry = index - 1;
       const currentPercentage = convertToPercent(current.value, total);
       const currrentDegrees = convertToDegrees(currentPercentage);
       const startDegrees = dataForDonut[lastEntry]?.end || INITIAL_DEGREE;
       const endDegrees = startDegrees + currrentDegrees;
+      if (i > COLORS.length - 1) {
+        i = 0;
+      }
+      const color = getColor(i, current.color);
+      i++;
 
       dataForDonut.push({
         name: current.name,
-        color: current.color,
+        color: color,
         start: startDegrees,
         end: endDegrees,
       });
--- src/donuts/simply-donut/types.ts
+++ src/donuts/simply-donut/types.ts
@@ -1,7 +1,7 @@
 export type TDonutData = {
   name: string;
   value: number;
-  color: string;
+  color?: string;
 };
 
 export type TSimplyDonut = {
@@ -19,3 +19,17 @@ export type TDataForDonut = {
 export const TOTAL_DEGREES = 360;
 export const TOTAL_PERCENTAGE = 100;
 export const INITIAL_DEGREE = 0;
+export const COLORS: string[] = [
+  "#FF5733",
+  "#FFBD33",
+  "#FFDD33",
+  "#C1FF33",
+  "#69FF33",
+  "#33FF82",
+  "#33FFDD",
+  "#33B5FF",
+  "#337AFF",
+  "#3349FF",
+  "#7433FF",
+  "#B533FF",
+];
